5692f052bb92507e0b7a486b485c017e9fdb2a7d
zhaoliang <zhaoliang01@hecom.cn>
2018-04-12 10:15:15 +0800

    去掉security

--- src/main/java/com/liang/seckill/service/SampleService.java
+++ src/main/java/com/liang/seckill/service/SampleService.java
@@ -1,13 +1,13 @@
 package com.liang.seckill.service;
-
+/*
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;*/
 import org.springframework.stereotype.Service;
 
 @Service
 public class SampleService {
 
-    @Secured("ROLE_USER")
+   /* @Secured("ROLE_USER")
     public String secure() {
         return "Hello Security";
     }
@@ -20,6 +20,6 @@ public class SampleService {
     @PreAuthorize("false")
     public String denied() {
         return "Goodbye World";
-    }
+    }*/
 
 }
